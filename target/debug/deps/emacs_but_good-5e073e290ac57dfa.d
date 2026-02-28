8ae0a157ef09c95d65cd516f738a43db03b7ac28
HonseGathGath <lassouedghaith15@gmail.com>
2026-02-28 08:01:06 +0100

    removed unnecassary lib.rs file

---
+++ target/debug/deps/emacs_but_good-5e073e290ac57dfa.d
@@ -0,0 +1,12 @@
+/home/ghaith/main/dev/emacs_but_good/target/debug/deps/emacs_but_good-5e073e290ac57dfa.d: src/main.rs src/editor.rs src/editor/terminal.rs src/editor/view.rs src/editor/view/buffer.rs
+
+/home/ghaith/main/dev/emacs_but_good/target/debug/deps/emacs_but_good-5e073e290ac57dfa: src/main.rs src/editor.rs src/editor/terminal.rs src/editor/view.rs src/editor/view/buffer.rs
+
+src/main.rs:
+src/editor.rs:
+src/editor/terminal.rs:
+src/editor/view.rs:
+src/editor/view/buffer.rs:
+
+# env-dep:CARGO_PKG_NAME=emacs_but_good
+# env-dep:CARGO_PKG_VERSION=0.1.0
